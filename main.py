41dbd48a6c981527bbbf0214497ff6b4e0a0ad19
Deepika Sharma <deepikaasharma@outlook.com>
2020-08-17 04:44:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+num=int(input("Enter a number: "))
+
+if num==10:
+  print('num== 10 resolves to True')
+elif num>20 and num < 50:
+  print("num>20 and num < 50 resolves to True")
+elif num==60:
+  print("num==60 resolves to True")
+else:
+  print("all of the prior conditions resolved to False")
